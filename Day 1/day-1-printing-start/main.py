d076bb6554b675c15eb9b3b75d16566e62a8040c
Tony <poorsun@gmail.com>
2022-08-02 09:39:52 -0500

    Completed Day 1

---
+++ Day 1/day-1-printing-start/main.py
@@ -0,0 +1,13 @@
+print("Hello world!")
+
+#new line
+print("Hello world!\nHello world!")
+
+#combine strings with +
+print("Hello" + " " + "Angela")
+
+#input function
+input("What is your name?")
+
+#print after gaining input
+print("Hello " + input("What is your name?") + "!")

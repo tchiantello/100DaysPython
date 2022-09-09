0523f4b0843e43b7850f0dd8c18dc1b074fbb33c
Tony <poorsun@gmail.com>
2022-09-09 15:43:00 -0500

    Began Day 8

---
+++ Day 8/function.py
@@ -0,0 +1,26 @@
+#Review
+#Create a function called greet()
+#Write 3 print statements inside the function
+# call the function to run the code
+
+# def greet():
+#     print("Hello")
+#     print("Bonjour")
+#     print("Hallo")
+
+# greet()
+
+#can add a variable to a functions "()"
+#create a funciton that allows for input
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do, {name}?")
+
+# greet_with_name("Angela")
+
+#Function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with(name="Angela", location="London")

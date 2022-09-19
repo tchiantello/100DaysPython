713add6d345da9afd1ddf60f57439aef1837b35e
tchiantello <70589704+tchiantello@users.noreply.github.com>
2022-09-19 09:18:34 -0500

    Completed Day 8

--- Day 8/Final Project/cipher1.py
+++ Day 8/Final Project/cipher1.py
@@ -1,18 +1,18 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
 def encrypt(text, shift):
+    message = ""
     for letter in text:
         index = alphabet.index(letter)
-        letter_shift = index + shift
-        new_letter = alphabet.index(letter_shift)
-
-        print(new_letter)
+        new_letter = alphabet[index + shift]
+        message += new_letter
+    print(f"The encoded text is {message}")
 
 encrypt(text, shift)
 
